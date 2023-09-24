f910b578bc24a07e1ff7c64d11e13be3899239f6
dwlsolutions <dwlmule@gmail.com>
2023-09-25 02:01:29 +0530

    REST API Call

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
-    // Load JSON data using jQuery
-    $.getJSON('./data.json', function (data) {
-        // Initialize DataTable
-        $('#dataTable').DataTable({
-            data: data, // Assuming data is an array of objects
-            columns: [
-                { data: 'item' },
-                { data: 'place' },
-                { data: 'rating' }
-            ]
-        });
+    // Make a REST API call to retrieve JSON data
+    $.ajax({
+        url: 'https://script.google.com/macros/s/AKfycbz6aNo8pfxeMx0r09jkAjE2J-ZJw4a2Blva4EG8NNqfQzhp0aKRAaJKwipvV9hYrbGSKQ/exec?path=dishes-table',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            // Initialize DataTable
+            $('#dataTable').DataTable({
+                data: data,
+                columns: [
+                    { data: 'item' },
+                    { data: 'place' },
+                    { data: 'rating' }
+                ]
+            });
+        },
+        error: function (error) {
+            console.error('Error fetching data from the API:', error);
+        }
     });
 });
+
