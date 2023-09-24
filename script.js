945a3902af9ab69c2a26a551e2df6d866581f80a
dwlsolutions <dwlmule@gmail.com>
2023-09-24 23:53:45 +0530

    table-style

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     // Load JSON data using jQuery
-    $.getJSON('data.json', function (data) {
+    $.getJSON('./data.json', function (data) {
         // Initialize DataTable
         $('#dataTable').DataTable({
             data: data, // Assuming data is an array of objects
             columns: [
-                { data: 'food_item' },
+                { data: 'item' },
                 { data: 'place' },
                 { data: 'rating' }
             ]
