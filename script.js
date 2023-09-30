9fc550ee5bade1e539c45c63b21a3742b640795c
dwlsolutions <dwlmule@gmail.com>
2023-09-30 13:32:46 +0530

    navbar

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     // Make a REST API call to retrieve JSON data
     $.ajax({
-        url: 'https://script.google.com/macros/s/AKfycbzlBsu7t_UJyh97_NpZV0bbuN0rquLOaP4XwVXfIqpQn0UhIqWh5nn6tRJhZ31VODbxIg/exec?path=dishes-table&subscription_key=jqZiBkxZayNRgQZpEU8ZlYF3EgNZRbJ3HY24bi',
+        url: 'https://dummy.restapiexample.com/api/v1/employees',
         type: 'GET',
         dataType: 'json',
         success: function (data) {
             // Initialize DataTable
             $('#dataTable').DataTable({
-                data: data,
+                data: data.data,
                 columns: [
-                    { data: 'item' },
-                    { data: 'place' },
-                    { data: 'rating' }
+                    { data: 'id' },
+                    { data: 'employee_name' },
+                    { data: 'employee_salary' }
                 ]
             });
         },
@@ -20,4 +20,6 @@ $(document).ready(function () {
         }
     });
 });
+
+
 
