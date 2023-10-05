62c55bc7016ef44b0a17e88dafdefc75af85d35a
dwlsolutions <dwlmule@gmail.com>
2023-10-05 21:27:15 +0530

    add-page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // Make a REST API call to retrieve JSON data
     $.ajax({
-        url: 'https://dummy.restapiexample.com/api/v1/employees',
+        url: 'https://script.google.com/macros/s/AKfycbzlBsu7t_UJyh97_NpZV0bbuN0rquLOaP4XwVXfIqpQn0UhIqWh5nn6tRJhZ31VODbxIg/exec?path=dishes-table&subscription_key=jqZiBkxZayNRgQZpEU8ZlYF3EgNZRbJ3HY24bi',
         type: 'GET',
         dataType: 'json',
         success: function (data) {
@@ -9,9 +9,9 @@ $(document).ready(function () {
             $('#dataTable').DataTable({
                 data: data.data,
                 columns: [
-                    { data: 'id' },
-                    { data: 'employee_name' },
-                    { data: 'employee_salary' }
+                    { data: 'item' },
+                    { data: 'place' },
+                    { data: 'rating' }
                 ]
             });
         },
